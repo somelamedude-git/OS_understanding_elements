c00ea9150deae5970eda3267400761453d62b41c
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-20 18:41:38 +0530

    array unflattened for solving purposes

--- c_level/resize.c
+++ c_level/resize.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int*** resized_matrix(int** matrix_one, int** matrix_two, int rows, int cols){
+int**** resized_matrix(int** matrix_one, int** matrix_two, int rows, int cols){
 	if(rows != cols){
 		return null;
 	}
 
 	int size_required = (rows*rows)/4;
-	int*** resized_container = new int**[size_required];
+	int**** resized_container = new int**[size_required];
 
 	for(int i =0; i<size_required; i++){
 		int** resized_arr = new int*[2];
 		resized_container[i] = resized_arr;
 	}
 
+	for(int i =0; i<rows; i++){
+		for(int j =0; j<cols; j++){
 
 }
