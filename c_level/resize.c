dd4bf201b0737b9949463142458b063fa858de6e
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-20 19:23:07 +0530

    major error fixed :)

--- c_level/resize.c
+++ c_level/resize.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-int**** resized_matrix(int** matrix_one, int** matrix_two, int rows, int cols){
+int**** resized_matrix( int** matrix, int rows, int cols){
 	if(rows != cols){
-		return null;
+		return NULL;
 	}
 
 	int size_required = rows/2;
-	int**** resized_container = new int***[size_required];
+	int**** resized_container =(int****) malloc(size_required* sizeof(int***));
 
 	for(int i =0; i<size_required; i++){
-		int** resized_arr = new int*[2];
+		int** resized_arr = (int**)malloc(sizeof(int*) *2);
 		resized_container[i] = resized_arr;
 	}
 
 	for(int i =0; i<rows; i++){
-		for(int j =0; j<cols; j++){
-			resized_container[i][j/2]
+		for(int j =0; j<cols; j=j+2){
+			int* arr = (int*) malloc(sizeof(int)*2);
+			arr[0] = matrix
+
+	
 
 }
