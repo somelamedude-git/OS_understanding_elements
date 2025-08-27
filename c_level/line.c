0359aaae9578cc1c4aeaa5f7714366beb8612ac0
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-27 15:56:48 +0530

    dsa finally started

--- c_level/line.c
+++ c_level/line.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // We can assume a linked list has been formed of people
 
-struct Node{
+typedef struct Node{
     Node* next;
-    int data;
-}
+} Node;
 
 // Now people will be forming a linked list with each other
 
 int getNumInLine(Node* person){
-    
+    if(!person->next) return 1;
+    else return 1 + getNumInLine(person->next);
 }
+
+Node* initNode(){
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->next = NULL;
+	return newNode;
+}
+
+int main(){
+	Node* person_one = initNode();
+	Node* person_two = initNode();
+	Node* person_three = initNode();
+
+	person_one->next = person_two;
+	person_two->next = person_three;
+
+	int index = getNumInLine(person_one);
+	printf("%d", index);
+
+	return 0;
+}
+
