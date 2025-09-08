e74b9fd47e203a9653d50d46d8e92d09a3729dcf
somelamedude-git <cozydeveloper843@gmail.com>
2025-09-09 03:39:10 +0530

    no

---
+++ c_level/minPathSum.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int min(int a, int b){
+	return (a>b)?b:a;
+}
+
+int minSum(int** grid, int rows, int cols, int i, int j){
+	if(i>=rows || j>=cols) return INT_MAX;
+	if(i == rows-1 && j==cols-1) return grid[i][j];
+	return grid[i][j] + min(minSum(grid, rows, cols, i+1, j), minSum(grid, rows, cols, i, j+1));
+}
+
+
+int main() {
+    int rows = 3, cols = 3;
+
+    // allocate grid
+    int** grid = (int**)malloc(sizeof(int*) * rows);
+    for(int i = 0; i < rows; i++) {
+        grid[i] = (int*)malloc(sizeof(int) * cols);
+    }
+
+    // fill grid
+    int temp[3][3] = {
+        {1, 3, 1},
+        {1, 5, 1},
+        {4, 2, 1}
+    };
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < cols; j++)
+            grid[i][j] = temp[i][j];
+
+    int result = minSum(grid, rows, cols, 0, 0);
+    printf("Minimum path sum = %d\n", result);
+
+    // free memory
+    for(int i = 0; i < rows; i++) free(grid[i]);
+    free(grid);
+
+    return 0;
+}
+
