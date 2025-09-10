397c6f6c03685f41ff0fb8866a72fe1491604ab6
somelamedude-git <cozydeveloper843@gmail.com>
2025-09-10 19:06:18 +0530

    memo code written

--- c_level/guess.c
+++ c_level/guess.c
@@ -5,15 +5,24 @@
 int max(int a, int b){
 	return (a>b)?a:b;
 }
-int numBreak(int n){
-	if(n==1) return 1;
 
+int assign_memo(int n){
+	int* memo = (int*)malloc(sizeof(int) * n);
+	for(int i =0; i<n; i++) memo[i] = -1;
+	return memo;
+}
+
+int numBreak(int n, int* memo){
+	if(n==1) return 1;
+	if(memo[n-1] != -1) return memo[n-1];
 	int answer = INT_MIN;
 	for(int i = 1; i< n; i++){
 		int num = i*numBreak(n-i);
 		int num_two = i * (n-i);
 		answer = max(answer, max(num, num_two));
 	}
+
+	memo[n-1] = answer;
 
 	return answer;
 }
