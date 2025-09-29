5dbd9b6959bdb323044b16f832691fdf97c259d2
somelamedude-git <cozydeveloper843@gmail.com>
2025-09-30 00:30:36 +0530

    rotations added

--- c_level/self_balancing.c
+++ c_level/self_balancing.c
@@ -5,6 +5,7 @@ typedef struct TreeNode{
 	TreeNode* left;
 	TreeNode* right;
 	int data;
+	int height;
 } TreeNode;
 
 int max(int a, int b){
@@ -24,6 +25,26 @@ int calculate_depth(TreeNode* head){
 
 int calculate_bf(TreeNode* one, TreeNode* two){
 	return (calculate_depth(one) - calculate_depth(two));
+}
+
+TreeNode* left_rotate(TreeNode* node){
+	TreeNode* node_right = node->right;
+	TreeNode* shift_node = node_right->left;
+
+	node_right->left = node;
+	node -> right = shift_node;
+
+	return node_right;
+}
+
+TreeNode* right_rotate(TreeNode* node){
+	TreeNode* node_left = node->left;
+	TreeNode* shift_node = node_left->right;
+
+	node_left->right = node;
+	node->left = shift_node;
+
+	return node_left;
 }
 
 
